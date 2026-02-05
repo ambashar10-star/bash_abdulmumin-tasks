d59219251a3053a15a6554b297e14335c257c5b4
ambashar10-star <ambashar10@gmail.com>
2026-02-05 16:40:22 +0200

    Resolved merge conflict in index.php

--- src/index.php
+++ src/index.php
@@ -1,10 +1,5 @@
 <?php
-<<<<<<< HEAD
 echo "<h1>hello world this is test, my name is Bashar Abdul Mumin</h1>";
 phpinfo();
 ?>
-=======
-echo "<h1>hello world this is test</h1>";
-//phpinfo();
-?>
->>>>>>> 1953825560255634e4ef346d9f82f2833d2fdb39
+
